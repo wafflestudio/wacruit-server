d69a88a5b9d741a3489635b4e043cbe47f17654f
shinhong_park <shp7724@naver.com>
2023-06-30 17:59:35 +0900

    Use Aws Secret Manager & Prepare for Deployment (#3)
    * Add code owners
    
    * Use aws secret manager
    
    ---------
    
    Co-authored-by: shinhong-park <shinhong.park@linecorp.com>

--- wacruit/src/database/config.py
+++ wacruit/src/database/config.py
@@ -1,5 +1,6 @@
 from pydantic import BaseSettings
 
+from wacruit.src.secrets import AWSSecretManager
 from wacruit.src.settings import settings
 
 
@@ -18,7 +19,18 @@ class DBConfig(BaseSettings):
 
     @property
     def url(self) -> str:
+        aws_secrets = AWSSecretManager()
+        if aws_secrets.is_available():
+            self.username = aws_secrets.get_secret("db_username")
+            self.password = aws_secrets.get_secret("db_password")
+            self.host = aws_secrets.get_secret("db_host")
+            self.port = int(aws_secrets.get_secret("db_port"))
+            self.name = aws_secrets.get_secret("db_name")
+
         return (
             f"mysql+mysqldb://{self.username}:{self.password}"
             f"@{self.host}:{self.port}/{self.name}"
         )
+
+
+db_config = DBConfig()
