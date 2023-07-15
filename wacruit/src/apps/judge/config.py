4e85df07796cd9165162a4003da92c310fc405bd
shinhong_park <shp7724@naver.com>
2023-07-15 15:50:27 +0900

    Give default value to judge api url from secret manager (#9)
    Co-authored-by: shinhong-park <shinhong.park@linecorp.com>

--- wacruit/src/apps/judge/config.py
+++ wacruit/src/apps/judge/config.py
@@ -1,5 +1,6 @@
 from pydantic import BaseSettings
 
+from wacruit.src.secrets import AWSSecretManager
 from wacruit.src.settings import settings
 
 
@@ -10,6 +11,12 @@ class JudgeAPIConfig(BaseSettings):
         case_sensitive = False
         env_prefix = "JUDGE_"
         env_file = settings.env_files
+
+    def __init__(self) -> None:
+        super().__init__()
+        aws_secrets = AWSSecretManager()
+        if aws_secrets.is_available():
+            self.url = aws_secrets.get_secret("judge_api_url")
 
 
 judge_api_config = JudgeAPIConfig()
