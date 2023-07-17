7b79dd6834a2e9dc96212c1f26438c56e3294940
minkyu97 <whizkyu@snu.ac.kr>
2023-07-17 20:28:22 +0900

    code_submission_result 테이블 추가

--- wacruit/src/apps/problem/models.py
+++ wacruit/src/apps/problem/models.py
@@ -13,6 +13,7 @@ from sqlalchemy.sql import func
 
 from wacruit.src.database.base import DeclarativeBase
 from wacruit.src.database.base import intpk
+from wacruit.src.database.base import str255
 
 if TYPE_CHECKING:
     from wacruit.src.apps.user.models import User
@@ -35,16 +36,35 @@ class CodeSubmission(DeclarativeBase):
     user_id: Mapped[int | None] = mapped_column(
         ForeignKey("user.id", ondelete="SET NULL")
     )
-    user: Mapped["User"] = relationship(back_populates="code_submissions")
     problem_id: Mapped[int | None] = mapped_column(
         ForeignKey("problem.id", ondelete="SET NULL")
     )
-    problem: Mapped["Problem"] = relationship(back_populates="submissions")
     token: Mapped[str] = mapped_column(String(255), nullable=False)
     create_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
         server_default=func.now(),  # pylint: disable=not-callable
     )
+
+    user: Mapped["User"] = relationship(back_populates="code_submissions")
+    problem: Mapped["Problem"] = relationship(back_populates="submissions")
+    results: Mapped[list["CodeSubmissionResult"]] = relationship(
+        back_populates="submission"
+    )
+
+
+class CodeSubmissionResult(DeclarativeBase):
+    __tablename__ = "code_submission_result"
+
+    submission_id: Mapped[int] = mapped_column(
+        ForeignKey("code_submission.id", ondelete="CASCADE"), primary_key=True
+    )
+    testcase_id: Mapped[int] = mapped_column(
+        ForeignKey("testcase.id", ondelete="CASCADE"), primary_key=True
+    )
+    token: Mapped[str255]
+
+    submission: Mapped["CodeSubmission"] = relationship(back_populates="results")
+    testcase: Mapped["TestCase"] = relationship(back_populates="submission_results")
 
 
 class TestCase(DeclarativeBase):
@@ -58,3 +78,6 @@ class TestCase(DeclarativeBase):
     stdin: Mapped[str] = mapped_column(Text, nullable=False)
     expected_output: Mapped[str] = mapped_column(Text, nullable=False)
     is_example: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+    submission_results: Mapped[list["CodeSubmissionResult"]] = relationship(
+        back_populates="testcase"
+    )
