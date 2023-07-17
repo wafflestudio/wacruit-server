3397693521ce9395366463dc8752d40f89b2a63f
minkyu97 <whizkyu@snu.ac.kr>
2023-07-18 00:28:12 +0900

    judge batch request 추가

--- wacruit/src/apps/judge/repositories.py
+++ wacruit/src/apps/judge/repositories.py
@@ -1,3 +1,5 @@
+from typing import Iterable
+
 from fastapi import Depends
 from httpx import AsyncClient
 
@@ -5,6 +7,9 @@ from . import get_judge_api_client
 from .schemas import JudgeCreateSubmissionRequest
 from .schemas import JudgeCreateSubmissionResponse
 from .schemas import JudgeGetSubmissionResponse
+
+DEFAULT_PARAMS = {"base64_encoded": False}
+DEFAULT_FIELDS = "stdout,stderr,compile_output,message,status,time,memory"
 
 
 class JudgeApiRepository:
@@ -16,29 +21,46 @@ class JudgeApiRepository:
     ) -> JudgeCreateSubmissionResponse:
         res = await self.client.post(
             url="/submissions",
-            params={"base64_encoded": False},
-            data=request.dict(),
+            params=DEFAULT_PARAMS,
+            json=request.dict(),
             timeout=60,
         )
+        print(res.text)
         res.raise_for_status()
         return JudgeCreateSubmissionResponse(**res.json())
 
-    async def list_submission(self) -> JudgeGetSubmissionResponse:
-        fields = "stdout,stderr,compile_output,message,status,time,memory"
+    async def create_batch_submissions(
+        self, requests: list[JudgeCreateSubmissionRequest]
+    ) -> list[JudgeCreateSubmissionResponse]:
+        batch_request_data = {"submissions": [request.dict() for request in requests]}
+        res = await self.client.post(
+            url="/submissions/batch",
+            params=DEFAULT_PARAMS,
+            json=batch_request_data,
+            timeout=60,
+        )
+        res.raise_for_status()
+        return [JudgeCreateSubmissionResponse(**v) for v in res.json()]
+
+    async def get_submission(self, token: str) -> JudgeGetSubmissionResponse:
         res = await self.client.get(
-            url="/submissions",
-            params={"base64_encoded": False, "fields": fields},
+            url=f"/submissions/{token}",
+            params={**DEFAULT_PARAMS, "fields": ",".join(DEFAULT_FIELDS)},
             timeout=60,
         )
         res.raise_for_status()
         return JudgeGetSubmissionResponse(**res.json())
 
-    async def get_submission(self, token: str) -> JudgeGetSubmissionResponse:
-        fields = "stdout,stderr,compile_output,message,status,time,memory"
+    async def get_batch_submissions(
+        self, tokens: Iterable[str] | str
+    ) -> list[JudgeGetSubmissionResponse]:
+        if isinstance(tokens, Iterable):
+            tokens = ",".join(tokens)
         res = await self.client.get(
-            url=f"/submissions/{token}?base64_encoded=false",
-            params={"base64_encoded": False, "fields": ",".join(fields)},
+            url="/submissions/batch",
+            params={"tokens": tokens, **DEFAULT_PARAMS, "fields": DEFAULT_FIELDS},
             timeout=60,
         )
         res.raise_for_status()
-        return JudgeGetSubmissionResponse(**res.json())
+        submissions = res.json()["submissions"]
+        return [JudgeGetSubmissionResponse(**v) for v in submissions]
