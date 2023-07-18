00e0c620642b750f8b62fc39b5fa8d11f34467de
minkyu97 <whizkyu@snu.ac.kr>
2023-07-19 01:22:02 +0900

    problem_repository 테스트

---
+++ wacruit/src/tests/problem/conftest.py
@@ -0,0 +1,72 @@
+from httpx import AsyncClient
+import pytest
+from sqlalchemy.orm import Session
+
+from wacruit.src.apps.judge import get_judge_api_client
+from wacruit.src.apps.judge.repositories import JudgeApiRepository
+from wacruit.src.apps.problem.models import Problem
+from wacruit.src.apps.problem.models import TestCase
+from wacruit.src.apps.problem.repositories import CodeSubmissionRepository
+from wacruit.src.apps.problem.repositories import ProblemRepository
+from wacruit.src.apps.problem.services import ProblemService
+from wacruit.src.database.connection import Transaction
+
+
+@pytest.fixture
+def problem(db_session: Session) -> Problem:
+    p = Problem(num=1, body="1번 문제입니다.")
+    db_session.add(p)
+    db_session.commit()
+    example_testcase = TestCase(
+        problem_id=p.id,
+        stdin="123",
+        expected_output="12345",
+        time_limit=1.0,
+        is_example=True,
+    )
+    db_session.add(example_testcase)
+    real_testcase = TestCase(
+        problem_id=p.id,
+        stdin="321",
+        expected_output="54321",
+        time_limit=2.0,
+        is_example=False,
+    )
+    db_session.add(real_testcase)
+    db_session.commit()
+    return p
+
+
+@pytest.fixture
+def problem_repository(db_session: Session):
+    return ProblemRepository(session=db_session, transaction=Transaction(db_session))
+
+
+@pytest.fixture
+def code_submission_repository(db_session: Session):
+    return CodeSubmissionRepository(
+        session=db_session, transaction=Transaction(db_session)
+    )
+
+
+@pytest.fixture
+async def judge_api_client():
+    return await anext(get_judge_api_client())
+
+
+@pytest.fixture
+def judge_api_repository(judge_api_client: AsyncClient):
+    return JudgeApiRepository(client=judge_api_client)
+
+
+@pytest.fixture
+def problem_service(
+    problem_repository: ProblemRepository,
+    code_submission_repository: CodeSubmissionRepository,
+    judge_api_repository: JudgeApiRepository,
+):
+    return ProblemService(
+        problem_repository=problem_repository,
+        code_submission_repository=code_submission_repository,
+        judge_api_repository=judge_api_repository,
+    )
