e7b4c3e3c77f88feb9e424f5b99789d1aefca1c8
MINKYU LEE <whizkyu@snu.ac.kr>
2023-07-21 14:05:48 +0900

    add https replace middleware to admin app (#17)

--- wacruit/src/admin/__init__.py
+++
@@ -1,7 +0,0 @@
-from sqladmin import Admin as DefaultAdmin
-
-from wacruit.src.settings import settings
-
-from .applications import HttpsAdmin
-
-Admin = DefaultAdmin if settings.is_local or settings.is_test else HttpsAdmin
