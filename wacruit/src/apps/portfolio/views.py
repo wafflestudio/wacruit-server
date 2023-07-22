0a60c583bf3de0b2854ee8bdaaebdf6898c63abb
fivessun <fivessun@ridi.com>
2023-07-22 22:18:58 +0900

    reflect review : use responses

--- wacruit/src/apps/portfolio/views.py
+++ wacruit/src/apps/portfolio/views.py
@@ -3,6 +3,7 @@ from typing import Annotated
 import fastapi
 
 from wacruit.src.apps.common.dependencies import CurrentUser
+from wacruit.src.apps.common.exceptions import responses_from
 from wacruit.src.apps.portfolio.exceptions import NumPortfolioLimitException
 from wacruit.src.apps.portfolio.exceptions import PortfolioNotFoundException
 from wacruit.src.apps.portfolio.schemas import PortfolioNameResponse
@@ -12,7 +13,7 @@ from wacruit.src.apps.portfolio.services import PortfolioService
 v1_router = fastapi.APIRouter(prefix="/v1/portfolios", tags=["portfolios"])
 
 
-@v1_router.get("/", response_model=list[PortfolioNameResponse])
+@v1_router.get("/")
 def get_list_of_portfolios(
     current_user: CurrentUser,
     portfolio_service: Annotated[PortfolioService, fastapi.Depends()],
@@ -20,7 +21,7 @@ def get_list_of_portfolios(
     return portfolio_service.get_portfolio_responses(current_user.id)
 
 
-@v1_router.get("/url/download/{file_name}")
+@v1_router.get("/url/download/{file_name}", responses=responses_from(PortfolioNotFoundException))
 def get_download_portfolio_url(
     current_user: CurrentUser,
     file_name: str,
@@ -35,7 +36,7 @@ def get_download_portfolio_url(
     return portfolio_service.get_presigned_url_for_get_portfolio(object_name)
 
 
-@v1_router.get("/url/upload/{file_name}")
+@v1_router.get("/url/upload/{file_name}", responses=responses_from(NumPortfolioLimitException))
 def get_upload_portfolio_url(
     current_user: CurrentUser,
     file_name: str,
@@ -50,7 +51,7 @@ def get_upload_portfolio_url(
     return portfolio_service.get_presigned_url_for_upload_portfolio(object_name)
 
 
-@v1_router.get("/url/delete/{file_name}")
+@v1_router.get("/url/delete/{file_name}", responses=responses_from(PortfolioNotFoundException))
 def get_delete_portfolio_url(
     current_user: CurrentUser,
     file_name: str,
