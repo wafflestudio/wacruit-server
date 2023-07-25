2b078550d70a8d187c039d6ca5941190416e81b4
Minkyu Lee <whizkyu@snu.ac.kr>
2023-07-25 21:01:14 +0900

    problem test 개선

--- wacruit/src/tests/problem/conftest.py
+++ wacruit/src/tests/problem/conftest.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from datetime import timedelta
+
 from httpx import AsyncClient
 import pytest
 from sqlalchemy.orm import Session
@@ -8,32 +11,47 @@ from wacruit.src.apps.problem.models import Problem
 from wacruit.src.apps.problem.models import TestCase
 from wacruit.src.apps.problem.repositories import ProblemRepository
 from wacruit.src.apps.problem.services import ProblemService
+from wacruit.src.apps.recruiting.models import Recruiting
 from wacruit.src.database.connection import Transaction
 
 
 @pytest.fixture
 def problem(db_session: Session) -> Problem:
-    p = Problem(num=1, body="1번 문제입니다.")
-    db_session.add(p)
+    recruiting = Recruiting(
+        name="2023-루키-리크루팅",
+        is_active=True,
+        from_date=datetime.today() - timedelta(days=7),
+        to_date=datetime.today() + timedelta(days=7),
+        description="2023 루키 리크루팅입니다.",
+    )
+    db_session.add(recruiting)
     db_session.commit()
+
+    problem = Problem(num=1, body="1번 문제입니다.", recruiting_id=recruiting.id)
+    db_session.add(problem)
+    db_session.commit()
+
     example_testcase = TestCase(
-        problem_id=p.id,
-        stdin="123",
-        expected_output="12345",
+        problem_id=problem.id,
+        stdin="example_input",
+        expected_output="example_output",
         time_limit=1.0,
         is_example=True,
     )
     db_session.add(example_testcase)
+    db_session.commit()
+
     real_testcase = TestCase(
-        problem_id=p.id,
-        stdin="321",
-        expected_output="54321",
+        problem_id=problem.id,
+        stdin="real_input",
+        expected_output="real_output",
         time_limit=2.0,
         is_example=False,
     )
     db_session.add(real_testcase)
     db_session.commit()
-    return p
+
+    return problem
 
 
 @pytest.fixture
