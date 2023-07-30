fd0c79e4efdeb3f544cee4857edf0b27137dfcbe
fivessun <fivessun@ridi.com>
2023-07-30 18:18:20 +0900

    use ssm for getting bucket name

--- wacruit/src/apps/portfolio/file/aws/config.py
+++ wacruit/src/apps/portfolio/file/aws/config.py
@@ -1,17 +1,24 @@
 from pydantic import BaseSettings
 
+from wacruit.src.secrets import AWSSecretManager
 from wacruit.src.settings import settings
 
 
 class S3PortfolioConfig(BaseSettings):
     bucket_name: str = ""
-    bucket_region: str = ""
 
     class Config:
         case_sensitive = False
         env_prefix = "S3_PORTFOLIO_"
 
         env_file = settings.env_files
+
+    def __init__(self):
+        super().__init__()
+        aws_secrets = AWSSecretManager()
+        self.bucket_region = "ap-northeast-2"
+        if aws_secrets.is_available():
+            self.bucket_name = aws_secrets.get_secret("portfolio_bucket_name")
 
 
 s3_config = S3PortfolioConfig()
