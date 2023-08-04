aaf3d2a4148b07042d0d54b59e4f5a824888459f
Minkyu Lee <whizkyu@snu.ac.kr>
2023-08-05 02:32:45 +0900

    rapid_api 연결

--- wacruit/src/apps/judge/connections.py
+++ wacruit/src/apps/judge/connections.py
@@ -6,5 +6,8 @@ from .config import judge_api_config
 
 
 async def get_judge_api_client() -> AsyncGenerator[AsyncClient, None]:
-    async with AsyncClient(base_url=judge_api_config.url) as client:
+    async with AsyncClient(
+        base_url=judge_api_config.url,
+        headers={"X-RapidAPI-Key": judge_api_config.api_key},
+    ) as client:
         yield client
