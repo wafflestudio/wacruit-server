3bcf9b0f1c2061ae61f8e0e2f36b24671d1c433f
fivessun <fivessun@ridi.com>
2023-08-05 16:51:42 +0900

    remove portfolio stat api

--- wacruit/src/apps/portfolio/__init__.py
+++ wacruit/src/apps/portfolio/__init__.py
@@ -1,10 +1,8 @@
 from fastapi.routing import APIRouter
 
 from wacruit.src.apps.portfolio.file.views import v1_router as file_router
-from wacruit.src.apps.portfolio.stat.views import v1_router as stat_router
 from wacruit.src.apps.portfolio.url.views import v1_router as url_router
 
 router = APIRouter(prefix="/v1/portfolios")
 router.include_router(file_router)
-router.include_router(stat_router)
 router.include_router(url_router)
