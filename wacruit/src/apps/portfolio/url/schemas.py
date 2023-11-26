d615c32dc066a09a68f7b84939676ad92b664405
fivessun <fivessun@ridi.com>
2023-11-26 19:14:44 +0900

    change type annotation

--- wacruit/src/apps/portfolio/url/schemas.py
+++ wacruit/src/apps/portfolio/url/schemas.py
@@ -7,13 +7,13 @@ from wacruit.src.database.base import str255
 class PortfolioUrlResponse(OrmModel):
     id: int
     url: str255
-    generation: str | None
+    generation: int | None
 
 
 class PortfolioUrlRequest(BaseModel):
     url: str255
-    generation: str | None
+    generation: int | None
 
 
 class PortfolioRequest(BaseModel):
-    generation: str
+    generation: int
