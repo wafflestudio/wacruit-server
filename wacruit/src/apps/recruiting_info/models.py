5f7a94bfb87fba3dbda7da051a54c42684d9ae2d
문재영 <152019282+MunJaeyoung@users.noreply.github.com>
2025-07-03 21:00:35 +0900

    feat: db 모델 정의 (#73)
    - member, project, history, recruting_info, review, seminar, sponsor,
    timeline, faq 테이블에 대해 모델 정의를 작성했습니다.
    - recruit와 recruiting_info 관계를 정의했습니다.
    - pre-commit hook에 있는 pycln 버전에 문제가 있어 2.5.0 버전으로 변경했습니다.
    - migration 파일을 추가했습니다.

---
+++ wacruit/src/apps/recruiting_info/models.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from typing import TYPE_CHECKING
+
+from sqlalchemy import DateTime
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+
+from wacruit.src.database.base import DeclarativeBase
+from wacruit.src.database.base import intpk
+from wacruit.src.database.base import str50
+
+if TYPE_CHECKING:
+    from wacruit.src.apps.recruiting.models import Recruiting
+
+
+class RecruitingInfo(DeclarativeBase):
+    __tablename__ = "recruiting_info"
+
+    id: Mapped[intpk]
+    info_num: Mapped[int]
+    title: Mapped[str50]
+    date_info: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
+    recruiting_id: Mapped[int] = mapped_column(ForeignKey("recruiting.id"))
+
+    recruiting: Mapped["Recruiting"] = relationship(back_populates="recruiting_info")
---
+++ wacruit/src/apps/timeline/models.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from sqlalchemy import DateTime
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+from wacruit.src.database.base import DeclarativeBase
+from wacruit.src.database.base import intpk
+from wacruit.src.database.base import str30
+from wacruit.src.database.base import str255
+
+
+class Timeline(DeclarativeBase):
+    __tablename__ = "timeline"
+
+    id: Mapped[intpk]
+    title: Mapped[str30]
+    content: Mapped[str255 | None]
+    category: Mapped[str30]
+    start_date: Mapped[datetime] = mapped_column(DateTime(timezone=True))
+    end_date: Mapped[datetime] = mapped_column(DateTime(timezone=True))
