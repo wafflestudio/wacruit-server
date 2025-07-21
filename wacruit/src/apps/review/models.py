49eef86d382a1607e2f57607abc2761e25726a34
Kyeongpyo Lee <70705786+deveroskp@users.noreply.github.com>
2025-07-21 22:00:52 +0900

    Hotfix: history response 수정 (#92)
    - HistoryResponse를 수정했습니다.
    - Project introduction과 Review content의 길이가 짧은 것 같아 수정했습니다

--- wacruit/src/apps/review/models.py
+++ wacruit/src/apps/review/models.py
@@ -9,6 +9,7 @@ from wacruit.src.database.base import DeclarativeBase
 from wacruit.src.database.base import intpk
 from wacruit.src.database.base import str30
 from wacruit.src.database.base import str255
+from wacruit.src.database.base import str1500
 
 if TYPE_CHECKING:
     from wacruit.src.apps.member.models import Member
@@ -19,7 +20,7 @@ class Review(DeclarativeBase):
 
     id: Mapped[intpk]
     title: Mapped[str30]
-    content: Mapped[str255]
+    content: Mapped[str1500]
     member_id: Mapped[int] = mapped_column(ForeignKey("member.id"))
 
     member: Mapped["Member"] = relationship(back_populates="review")
